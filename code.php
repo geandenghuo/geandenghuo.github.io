80b42182a9343ddd7299d8b7c0fb9cd32c9b978f
geandenghuo <61575957+geandenghuo@users.noreply.github.com>
2021-04-03 13:29:54 +0800

    Delete code.php

--- code.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-define('ROOT_PATH', dirname(__FILE__));
-require '../if/ValidateCode.class.php';
-$_vc = new ValidateCode();
-$_vc->doimg();
-$_SESSION['vc_code'] = $_vc->getCode();
-?>
